fa41fd663a135863743942e303c3b08f4ff6d1a9
Sagore76 <81535674+Sagore76@users.noreply.github.com>
2026-05-23 13:47:38 +0200

    Añadir App básica React

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{padding:40,fontFamily:'Arial'}}>
+      <h1>Quedadas Amigos</h1>
+      <p>La aplicación ya está funcionando 🚀</p>
+    </div>
+  )
+}
